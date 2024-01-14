5974e4d173521cc6e82c102df4338543fd428df5
AbaevB <abaev70@hotmail.com>
2024-01-14 19:04:45 +0700

    add buyHealh buyWeapon and goTown functions

--- DragonRepeller/script.js
+++ DragonRepeller/script.js
@@ -16,16 +16,22 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
-function goStore(){
-  console.log("Going to store.");
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
 }
 
 function goCave(){
-  console.log("Going to cave.");
+  console.log("Go cave");
 }
 
 function fightDragon(){
-  console.log("Fighting dragon.");
+  console.log("Fighting dragon");
 }
 
 // initialize buttons
