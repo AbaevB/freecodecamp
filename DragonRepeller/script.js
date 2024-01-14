4ddc71cf2a28f5136728771dd95d81b86b8133a6
AbaevB <abaev70@hotmail.com>
2024-01-15 01:57:26 +0700

    add function goFight and start edit function fightSlime

--- DragonRepeller/script.js
+++ DragonRepeller/script.js
@@ -132,14 +132,14 @@ function buyWeapon() {
 
 }
 
-function sellWeapon(){
-  if(inventory.length > 1){
+function sellWeapon() {
+  if (inventory.length > 1) {
     gold += 15;
     goldText.innerText = gold;
     let currentWeapon = inventory.shift();
     text.innerText = "You sold a " + currentWeapon + ".";
     text.innerText += " In your inventory you have: " + inventory + ".";
-  }else{
+  } else {
     text.innerText = "Don't sell your only weapon!";
   }
 }
@@ -149,10 +149,15 @@ function fightDragon() {
 }
 
 function fightSlime() {
-
+  fighting = 0;
+  goFight();
 }
 
 function fightBeast() {
+
+}
+
+function goFight() {
 
 }
 // initialize buttons
