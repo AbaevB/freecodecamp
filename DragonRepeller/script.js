e25f63ce42e39b9afe22c03479ca35dffa5f24f9
AbaevB <abaev70@hotmail.com>
2024-01-15 04:17:07 +0700

    function goFight complete

--- DragonRepeller/script.js
+++ DragonRepeller/script.js
@@ -175,6 +175,10 @@ function fightDragon() {
 
 function goFight() {
   update(locations[3]);
+  let monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsters[fighting].health;
 }
 
 function attack(){
